1159788553a2e1699a2636695ed290bd900ad0aa
idn7 <idn7@cornell.edu>
2025-11-15 22:28:23 -0500

    Fixed with batching and simplicity

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,24 +1,15 @@
-import llama_cpp
 from llama_cpp import Llama
 import json
-import math
 
 BATCH_SIZE = 50
 embedder = Llama(
     model_path="model/bge-base-en-v1.5-f32.gguf",
     n_gpu_layers=-1,
-    n_ctx=512,
     embedding=True,
-    verbose=False,
     n_batch=BATCH_SIZE,
     logits_all=False,
-    pooling_type=llama_cpp.LLAMA_POOLING_TYPE_MEAN
 )
 
-
-def embed_batch(texts):
-    result = embedder.create_embedding(texts)
-    return [entry["embedding"] for entry in result["data"]]
 
 if __name__ == "__main__":
     print("-- Opening 'data/documents.json' --")
@@ -31,8 +22,9 @@ if __name__ == "__main__":
     all_embeddings = []
     
     for i in range(0, len(texts), BATCH_SIZE):
-        batch_texts = texts[i * BATCH_SIZE : (i + 1) * BATCH_SIZE]
-        all_embeddings.extend(embed_batch(batch_texts))
+        batch_texts = texts[i:i+BATCH_SIZE]
+        all_embeddings.extend(embedder.embed(batch_texts))
+        
     for doc, emb in zip(docs, all_embeddings):
         doc["embedding"] = emb
     print("-- Embedding Complete --")
